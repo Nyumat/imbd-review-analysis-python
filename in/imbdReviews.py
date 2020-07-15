1bd99375908aaf4d12f510b371bb0c6d0d2f940c
thomas <46255836+Nyumat@users.noreply.github.com>
2020-07-15 04:27:45 -0400

    Update imbdReviews.py

--- in/imbdReviews.py
+++ in/imbdReviews.py
@@ -25,81 +25,82 @@ driver.find_element_by_name("btnK").send_keys(Keys.ENTER)
 time.sleep(1)
 
 #Click the first URL shown, given a search page.
-driver.implicitly_wait(1)
+driver.implicitly_wait(10)
 driver.find_element_by_class_name("r").click()
-driver.implicitly_wait(1)
+driver.implicitly_wait(10)
 
-#Go to the user reviews page
+#Click the user reviews
+driver.find_element_by_xpath("/html/body/div[3]/div/div[2]/div[5]/div[1]/div/div/div[1]/div[1]/div[1]/a[3]").click()
 
-driver.find_elements_by_xpath("/html/body/div[2]/div/div[2]/div/div[1]/div/div/div[1]/div[1]/div[1]/a[3]")
-
-#Use B.S to scrap the IMBD review page
+#Scrap IMBD review
 ans = driver.current_url
 page = requests.get(ans)
 soup = BeautifulSoup(page.content, "html.parser")
 all = soup.find(id="main")
 
-#Scrap imbd Review
-parent = soup.find(class_="parent")
-name = soup.find(itemprop = "name")
-url = soup.find(itemprop = 'url')
-film_title = soup.get_text()
+#Get the title of the movie
+all = soup.find(id="main")
+parent = all.find(class_ ="parent")
+name = parent.find(itemprop = "name")
+url = name.find(itemprop = 'url')
+movie_title = url.get_text()
 
-# Get the title of the review
+#Get the title of the review
 title_rev = all.select(".title")
 title = [t.get_text().replace("\n", "") for t in title_rev]
 
-#Get the review itself
+#Get the review
 review_rev = all.select(".content .text")
 review = [r.get_text() for r in review_rev]
 
-#Turn it into a dataframe
+#Make it into dataframe
 table_review = pd.DataFrame({
     "Title" : title,
     "Review" : review
 })
 
-#Actual Review Analysis Time :p
+#Sentiment Analysis
+
 analyser = SentimentIntensityAnalyzer()
 sentiment1 = []
 sentiment2 = []
 
 for rev in review:
     score1 = analyser.polarity_scores(rev)
-    com_score = score1.get('Compound')
-    if com_score >= 0.05:
-        sentiment1.append('Positive')
+    com_score = score1.get('compound')
+    if com_score  >= 0.05:
+        sentiment1.append('positive')
     elif com_score > -0.05 and com_score < 0.05:
-        sentiment1.append('Neutral')
+        sentiment1.append('neutral')
     elif com_score <= -0.05:
-        sentiment1.append('Negative')
+        sentiment1.append('negative')
+
 table_review['Sentiment Vader'] = sentiment1
 
 #TextBlob
 for rev in review:
     score2 = TextBlob(rev).sentiment.polarity
     if score2 >= 0:
-        sentiment2.append('Positive')
+        sentiment2.append('positive')
     else:
-        sentiment2.append('Negative')
-#Print spammage :/ 
-print(f"The movie title is {film_title}")
+        sentiment2.append('negative')
+print(f"The movie title is {movie_title}")
 print("")
-print("According to vadersentiemnt, you should :")
-if sentiment1.count('Positive') > sentiment1.count('Negative'):
+print("According to Vadersentiemnt, you should :")
+if sentiment1.count('positive') > sentiment1.count('negative'):
     print('WATCH IT!')
 else:
     print("DON'T WATCH IT...")
-print('Positive : ', sentiment1.count('Positive'))
-print('Negative : ', sentiment1.count('Negative'))
+print('Positive : ', sentiment1.count('positive'))
+print('Negative : ', sentiment1.count('negative'))
 print("")
 print("According to TextBlob, you should :")
-if sentiment2.count('Positive') > sentiment2.count('Negative'):
+if sentiment2.count('positive') > sentiment2.count('negative'):
     print('WATCH IT!')
 else:
     print("DON'T WATCH IT...")
-print('Positive : ', sentiment2.count('Positive'))
-print('Negative : ', sentiment2.count('Negative'))
+print('Positive : ', sentiment2.count('positive'))
+print('Negative : ', sentiment2.count('negative'))
 
-#Close the browser
+#Close google chrome
 driver.close()
